8816d878402d0853cf97a92ae13d73e692fe4763
Jin Heo <993jin@gmail.com>
2021-04-12 13:04:25 -0400

    Add AmmoPack item scripts and rotation scripts for items

---
+++ Assets/Scripts/AmmoPack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPack : MonoBehaviour, IItem
+{
+    public int ammo = 30;
+
+    public void Use(GameObject target)
+    {
+        PlayerGun playerGun = target.GetComponent<PlayerGun>();
+        if(playerGun.gun && playerGun != null) playerGun.gun.currentAmmo += ammo;
+        Destroy(gameObject);
+    }
+}
+
