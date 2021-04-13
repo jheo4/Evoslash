9aa959e3d104fb47391cd635ec60dafa6692af7b
akvh3 <andrewvon@gatech.edu>
2021-04-13 15:59:37 -0400

    temporary jump fix

---
+++ Assets/Scripts/LockYPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockYPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,0,transform.position.z);
+        // if(transform.position.y >= 0.3) {
+        //     transform.position = new Vector3(transform.position.x,0,transform.position.z);
+        // }
+    }
+}
