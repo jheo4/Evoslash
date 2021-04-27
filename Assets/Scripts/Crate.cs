946709f81e166de4bb8775a44a3882fbfe8f4ed8
Jin Heo <993jin@gmail.com>
2021-04-27 16:23:11 -0400

    Add Crate scripts and update prefab

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public AudioClip hitSound;
+    private AudioSource audioPlayer;
+
+    void Awake()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        audioPlayer.PlayOneShot(hitSound, 0.5f);
+    }
+}
+
